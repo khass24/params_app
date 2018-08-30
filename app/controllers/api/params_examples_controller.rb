063f22453fa097ac6a1d36ace35ee17a4a100a66
Kyle Hassett <kylehassett72@gmail.com>
2018-08-29 21:17:43 -0500

    In class params app

---
+++ app/controllers/api/params_examples_controller.rb
@@ -0,0 +1,12 @@
+class Api::ParamsExamplesController < ApplicationController
+  def query_params_action
+    @input_message = params["my_message"]
+    @street_sweeper = params["taco"]
+    render 'query_params_view.json.jbuilder'
+  end
+
+  def segment_params_action
+    @input_value = params["this_is_a_key"]
+    render "segment_params_view.json.jbuilder"
+  end
+end
